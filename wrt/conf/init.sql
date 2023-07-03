1a5071ba5b14d789e7de875109185fada057c02a
Nick Kunz <nick.kunz@me.com>
2023-07-03 14:19:39 -0700

    Change TEXT data type to VARCHAR

--- wrt/conf/init.sql
+++ wrt/conf/init.sql
@@ -1,13 +1,19 @@
-CREATE TABLE IF NOT EXISTS idle (
-    id SERIAL PRIMARY KEY,
-    vehicle_id TEXT,
-    trip_id TEXT,
-    route_id TEXT,
+CREATE TABLE IF NOT EXISTS agency (
+    iata_id VARCHAR(255),
+    agency VARCHAR(255),
+    city VARCHAR(255),
+    country VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS events (
+    iata_id VARCHAR(255),
+    vehicle_id VARCHAR(255),
+    trip_id VARCHAR(255),
+    route_id VARCHAR(255),
     latitude FLOAT,
     longitude FLOAT,
     datetime INT,
     duration INT,
-    source TEXT,
     CONSTRAINT unique_columns UNIQUE (
         vehicle_id,
         trip_id,
