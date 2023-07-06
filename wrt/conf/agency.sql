a4f06c50d70e323a2cb29497d26fd24cb826d01b
Nick Kunz <nick.kunz@me.com>
2023-07-06 08:40:28 -0700

    Prevent agency entry duplicates

--- wrt/conf/agency.sql
+++ wrt/conf/agency.sql
@@ -1,13 +1,13 @@
-INSERT INTO agency (
-  iata_id,
-  agency,
-  city,
-  country
+WITH j (iata_id, agency, city, country) AS (
+  VALUES 
+    ('NYC', 'Metropolitan Transportation Authority (MTA)', 'New York', 'United States'),
+    ('BOS', 'Massachusetts Bay Transportation Authority (MBTA)', 'Boston', 'United States'),
+    ('PHL', 'Southeastern Pennsylvania Transportation Authority (SEPTA)', 'Philadelphia', 'United States'),
+    ('PIT', 'Pittsburgh Regional Transit (PRT)', 'Pittsburgh', 'United States'),
+    ('SFO', 'Bay Area Rapid Transit (BART)', 'San Francisco', 'United States')
 )
-VALUES 
-  ('NYC', 'Metropolitan Transportation Authority (MTA)', 'New York', 'United States'),
-  ('BOS', 'Massachusetts Bay Transportation Authority (MBTA)', 'Boston', 'United States'),
-  ('PHL', 'Southeastern Pennsylvania Transportation Authority (SEPTA)', 'Philadelphia', 'United States'),
-  ('PIT', 'Pittsburgh Regional Transit (PRT)', 'Pittsburgh', 'United States'),
-  ('SFO', 'Bay Area Rapid Transit (BART)', 'San Francisco', 'United States')
-ON CONFLICT DO NOTHING;
+INSERT INTO agency (iata_id, agency, city, country)
+SELECT i.iata_id, i.agency, i.city, i.country
+FROM j i
+LEFT JOIN agency k ON k.iata_id = i.iata_id
+WHERE k.iata_id IS NULL;
