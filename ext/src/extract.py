15032da71957a046a9250caa572ea82a9e19c535
Nick Kunz <nick.kunz@me.com>
2023-11-03 08:40:40 -0700

    Enhance request param handling and header response, relax entity valid

--- ext/src/extract.py
+++ ext/src/extract.py
@@ -1,19 +1,101 @@
 ## libraries
+import os
 import sys
-import base64
+import logging
 import aiohttp
 import asyncio
+import configparser
+from dotenv import load_dotenv
 from google.transit import gtfs_realtime_pb2
 
 ## modules
 sys.path.insert(0, './')
-from conf.conf import ini_key, env_var
+
+## params
+LOG_LEVEL = str(os.getenv('LOG_LEVEL', 'INFO'))
+
+## logging config
+logging.basicConfig(
+    level = LOG_LEVEL,
+    format = '%(asctime)s - %(levelname)s - %(message)s'
+)
 
 ## fix for selectors bug
 import selectors
-selectors._PollLikeSelector.modify = (
-    selectors._BaseSelectorImpl.modify
-)
+selectors._PollLikeSelector.modify = (selectors._BaseSelectorImpl.modify)
+
+## ini section keys
+def ini_key(file, sect = None):
+
+    """
+    Desc:
+        Loads a configuration variables from specified section in .ini file and 
+        returns them as a dictionary. If no section is specified, it returns all 
+        of the variables in the .ini file.
+
+    Args:
+        file (str): Path of the .ini configuration file.
+        sect (str): Name of the section witin the .ini file. Default is None.
+
+    Returns:
+        dict: A dictionary containing the key-value pairs of the specified section, 
+        or all key-value pairs in the .ini file if no section is specified.
+
+    Raises:
+        TypeError: The argument 'file' is not a string.
+        TypeError: The argument 'sect' is not a string.
+    """
+
+    ## arg check
+    if not isinstance(file, str):
+        raise TypeError("The 'file' argument must be a string.")
+
+    if sect is not None and not isinstance(sect, str):
+        raise TypeError("The 'sect' argument must be a string or None.")
+
+    ## input ini file 
+    config = configparser.ConfigParser()
+    config.optionxform = lambda option: option  ## preserve uppercase vars
+    config.read(file)
+
+    ## output keys
+    keys = dict()
+    for i in config[sect]:
+        value = config.get(sect, i)
+        keys[i] = value
+
+    return keys
+
+## env variables
+def env_var(file):
+
+    """
+    Desc:
+        Loads environment variables from .env file and returns them as a dictionary.
+
+    Args:
+        file (str): The path of the .env file.
+
+    Returns:
+        dict: A dictionary containing the environment variables.
+
+    Raises:
+        TypeError: The argument 'file' is not a string.
+    """
+
+    ## arg check
+    if not isinstance(file, str):
+        raise TypeError("The 'file' argument must be a string.")
+
+    ## load env vars
+    load_dotenv(file)
+
+    ## output vars
+    vars = dict()
+    for i in os.environ:
+        vars[i] = os.environ[i]
+
+    return vars
 
 ## extract data from source
 class ExtractData():
@@ -21,21 +103,20 @@ class ExtractData():
 
         """
         Desc:
-            Extracts GTFS Real-Time telemetry vehicle positions directly from 
-            specified transportation agency API's.
+            Extracts GTFS Realtime feeds from specified transit agency REST API endpoints.
 
         Args:
             env_file (str): Path to .env file containing environment variables.
-            ini_file (str): Path to .ini configuration file.
-            ini_sect (str): Section of the .ini configuration file to use.
+            ini_file (str): Path to .ini configuration file containing API endpoints.
+            ini_sect (str): Section of the .ini configuration file to use (optional).
 
         Returns:
             A tuple with three elements:
-            - A serialized protobuf message containing real-time transit data.
-            - An HTTP status code (always 200).
-            - A dictionary with a single key-value pair indicating that the 
-              message is in the 'application/x-protobuf' format.
-        
+            - A serialized protobuf message respons.
+            - An HTTP status code (200 good, 500 error).
+            - A dictionary with three key-value pairs indicating content type, content
+              length, and connection type.
+
         Raises:
             TypeError: If any of the arguments are not a string.
         """
@@ -43,140 +124,274 @@ class ExtractData():
         ## arg check
         if not isinstance(env_file, str):
             raise TypeError("env_file arg must be a string")
-        
         if not isinstance(ini_file, str):
             raise TypeError("ini_file arg must be a string")
-
         if not isinstance(ini_sect, str):
             raise TypeError("ini_sect arg must be a string")
 
         self.env_file = env_file
         self.ini_file = ini_file
         self.ini_sect = ini_sect
+        self.key_last = dict()
 
         ## api keys
         self.keys = env_var(
             file = self.env_file
         )
 
-        ## api urls (data sources)
+        ## api endpoints (gtfs realtime feeds)
         self.urls = ini_key(
             file = self.ini_file,
             sect = self.ini_sect
         )
 
-        ## add api key to urls
-        self.add_key()
+    ## toggle between two api keys
+    def alt_key(self, url, keys):
+        if url not in self.key_last:
+            self.key_last[url] = keys[0]  ## default first key
 
-    ## add api key to urls
-    def add_key(self):
-        for k in self.urls.keys():
-            if k.startswith('url_'):
-                key = k.upper()[4:] + '_API_KEY'
-                if key in self.keys:
-                    self.urls[k] += self.keys[key]
+        ## alternating logic
+        key_a = self.key_last[url]
+        key_b = keys[1] if key_a == keys[0] else keys[0]
+        self.key_last[url] = key_b
+        return key_b
 
     ## extract data
     async def ext_dat(self):
-        try:
-            async with aiohttp.ClientSession(trust_env = True) as sess:
-                conn = list()
-                for k, i in self.urls.items():
 
-                    ## los angeles and baltimore and miami auth headers
-                    if k in ['url_lax', 'url_bwi', 'url_mia']:
-                        head = {'Authorization': self.keys['LBM_API_KEY']}
-                        conn.append(
-                            sess.get(i, headers = head)
+        ## create session
+        headers_master = {
+            'User-Agent': 'GRD-TRT-BUF-4I/1.0',
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Connection': 'keep-alive',
+            'Cache-Control': 'no-cache'
+        }
+        connector = aiohttp.TCPConnector(
+            ssl = True if 'https' in self.urls else False
+        )
+        async with aiohttp.ClientSession(
+            connector = connector,
+            trust_env = True
+            ) as session:
+
+            ## add auth headers for cities
+            connection = list()
+            for i, url in self.urls.items():
+                headers = headers_master.copy()
+
+                ## new york
+                if i == 'API_END_NYC':
+                    params = {'key': self.keys['API_KEY_NYC']}
+                    connection.append(
+                        session.get(
+                            url = url,
+                            headers = headers,
+                            params = params
                         )
-
-                    ## washington dc auth headers
-                    elif k == 'url_dca':
-                        head = {'api_key': self.keys['DCA_API_KEY']}
-                        conn.append(
-                            sess.get(i, headers = head)
+                    )
+                ## wash dc
+                elif i == 'API_END_DCA':
+                    headers['api_key'] = self.keys['API_KEY_DCA']
+                    connection.append(
+                        session.get(
+                            url = url,
+                            headers = headers
                         )
-
-                    ## montreal auth headers
-                    elif k == 'url_yul':
-                        head = {'apiKey': self.keys['YUL_API_KEY']}
-                        conn.append(
-                            sess.get(i, headers = head)
+                    )
+                ## los angeles, miami
+                elif i in ['API_END_LAX', 'API_END_MIA']:
+                    headers['Authorization'] = self.keys['API_KEY_LBM']
+                    connection.append(
+                        session.get(
+                            url = url,
+                            headers = headers
                         )
-
-                    ## sydney auth headers
-                    elif k == 'url_syd':
-                        head = {'Authorization': 'apikey ' + self.keys['SYD_API_KEY']}
-                        conn.append(
-                            sess.get(i, headers = head)
+                    )
+                ## san fran
+                elif i == 'API_END_SFO':
+                    params = {
+                        'api_key': self.keys['API_KEY_SFO'],
+                        'agency': 'SF'
+                    }
+                    connection.append(
+                        session.get(
+                            url = url,
+                            headers = headers,
+                            params = params
                         )
-
-                    ## other cities no auth headers
-                    else:
-                        conn.append(
-                            sess.get(i)
+                    )
+                ## portland
+                elif i == 'API_END_PDX':
+                    params = {'appID': self.keys['API_KEY_PDX']}
+                    connection.append(
+                        session.get(
+                            url = url,
+                            headers = headers,
+                            params = params
                         )
+                    )
+                ## phoenix
+                elif i == 'API_END_PHX':
+                    params = {'apiKey': self.keys['API_KEY_PHX']}
+                    connection.append(
+                        session.get(
+                            url = url,
+                            headers = headers,
+                            params = params
+                        )
+                    )
+                ## montreal
+                elif i == 'API_END_YUL':
+                    params = {'token': self.keys['API_KEY_YUL']}
+                    connection.append(
+                        session.get(
+                            url = url,
+                            headers = headers,
+                            params = params
+                        )
+                    )
+                ## vancouver
+                elif i == 'API_END_YVR':
+                    params = {'apikey': self.keys['API_KEY_YVR']}
+                    connection.append(
+                        session.get(
+                            url = url,
+                            headers = headers,
+                            params = params
+                        )
+                    )
+                ## stockholm
+                elif i == 'API_END_ARN':
+                    params = {'key': self.keys['API_KEY_ARN']}
+                    connection.append(
+                        session.get(
+                            url = url,
+                            headers = headers,
+                            params = params
+                        )
+                    )
+                ## dublin
+                elif i == 'API_END_DUB':
+                    keys = [self.keys['API_KEY_DUB_A'], self.keys['API_KEY_DUB_B']]
+                    key = self.alt_key(  ## toggle api keys
+                        url = url,
+                        keys = keys
+                    )
+                    headers['x-api-key'] = key
+                    connection.append(
+                        session.get(
+                            url=url,
+                            headers=headers
+                        )
+                    )
+                ## sydney
+                elif i == 'API_END_SYD':
+                    headers['Authorization'] = 'apikey' + ' ' + self.keys['API_KEY_SYD']
+                    connection.append(
+                        session.get(
+                            url = url,
+                            headers = headers
+                        )
+                    )
+                ## auckland
+                elif i == 'API_END_AKL':
+                    headers['Ocp-Apim-Subscription-Key'] = self.keys['API_KEY_AKL']
+                    headers['Accept'] = 'application/x-protobuf'  ## required to return protobufs
+                    connection.append(
+                        session.get(
+                            url = url,
+                            headers = headers
+                        )
+                    )
+                ## christchurch
+                elif i == 'API_END_CHC':
+                    headers['Ocp-Apim-Subscription-Key'] = self.keys['API_KEY_CHC']
+                    connection.append(
+                        session.get(
+                            url = url,
+                            headers = headers
+                        )
+                    )
+                ## delhi
+                elif i == 'API_END_DEL':
+                    params = {'key': self.keys['API_KEY_DEL']}
+                    connection.append(
+                        session.get(
+                            url = url,
+                            headers = headers,
+                            params = params
+                        )
+                    )
+                ## other cities without auth headers
+                else:
+                    connection.append(
+                        session.get(
+                            url = url,
+                            headers = headers_master
+                        )
+                    )
 
-                ## join requests
-                resps = await asyncio.gather(
-                    *conn,
-                    return_exceptions = False
-                )
+            ## log request headers before sending requests
+            logging.debug(f"Request headers for ({url}): {headers}")
 
-                ## process responses
-                feeds = bytes()
-                for url, i in enumerate(resps):
-                    content = await i.content.read()
+            ## join requests and process responses
+            response = await asyncio.gather(*connection)
+            feeds = bytes()
+            for url, k in enumerate(response):
 
-                    ## parse protobuf
-                    message = gtfs_realtime_pb2.FeedMessage()
-                    message.ParseFromString(bytes(content))
+                ## log response status
+                url_log = list(self.urls.values())[url]
+                if isinstance(k, Exception):
+                    logging.error(
+                        msg = f"GET request to {url_log} failed with exception: {k}"
+                    )
+                    continue
+                if k.status != 200:
+                    logging.warning(
+                        msg = f"GET request to {url_log} unsuccessful with HTTP status code {k.status}."
+                    )
+                    continue  ## proceeds to next url upon exception
+                else:
+                    logging.info(
+                        msg = f"GET request to {url_log} successful with HTTP status code {k.status}."
+                    )
+                    content = await k.content.read()
 
-                    # validate header
-                    header = message.header
-                    if (header.gtfs_realtime_version == '2.0' or \
-                        header.gtfs_realtime_version == '1.0') and \
-                        header.incrementality == gtfs_realtime_pb2.FeedHeader.FULL_DATASET:
+                ## parse protobuf
+                message = gtfs_realtime_pb2.FeedMessage()
+                message.ParseFromString(bytes(content))
 
-                            ## validate entity
-                            entity = message.entity
-                            entity_valid = [j for j in entity if (
-                                j.vehicle.vehicle.id and 
-                                j.vehicle.trip.route_id and 
-                                j.vehicle.trip.trip_id and 
-                                j.vehicle.timestamp and 
-                                j.vehicle.position.latitude and 
-                                j.vehicle.position.longitude
-                                )
-                            ]
+                # validate message header
+                if (message.header.gtfs_realtime_version == '2.0' or \
+                    message.header.gtfs_realtime_version == '1.0') and \
+                    message.header.incrementality == gtfs_realtime_pb2.FeedHeader.FULL_DATASET:
 
-                            ## use vehicle label field as iata id
-                            url_key = list(self.urls.keys())[url]
-                            for j in entity_valid:
-                                j.vehicle.vehicle.label = str(url_key[-3:]).upper()
+                    valid_entity = [j for j in message.entity if (
+                        j.vehicle.vehicle.id and \
+                        j.vehicle.timestamp and \
+                        j.vehicle.position.latitude and \
+                        j.vehicle.position.longitude and \
+                        (j.vehicle.trip.route_id or j.vehicle.trip.trip_id)
+                        )
+                    ]
+                    url_key = list(self.urls.keys())[url]
+                    for j in valid_entity:
+                        j.vehicle.vehicle.label = url_key.upper()[-3:]
 
-                            ## validation end
-                            del message.entity[:]
-                            message.entity.extend(entity_valid)
+                    ## end message validation
+                    del message.entity[:]
+                    message.entity.extend(valid_entity)
 
-                            ## serialize message and append to feed
-                            feeds += message.SerializeToString()
+                    ## serialize message, append to feed, update content length
+                    feeds += message.SerializeToString()
+                    content_length = len(feeds)
 
-                    ## release the connection
-                    await i.release()
-
-                ## close the session
-                await sess.close()
-
-                ## protobuf, http code, content type
-                return feeds, 200, {
-                    'Content-Type': 'application/x-protobuf'
-                }
-
-        ## session error
-        except:
-            return b'', 500, {
-                'Content-Type': 'application/x-protobuf'
+            ## successful protobuf response, http status, headers (strict order)
+            return feeds, 200, {
+                'Content-Type': 'application/x-protobuf',
+                'Content-Length': str(content_length),
+                'Connection': 'keep-alive'
             }
 
-## end of program
+## end program
