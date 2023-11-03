82bc582ae00a34f693b0f2bfee5d333cdfbe8df9
Nick Kunz <nick.kunz@me.com>
2023-11-03 08:33:16 -0700

    Turn off logging features in gunicorn

--- ext/start.sh
+++ ext/start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+set -e
 /etc/init.d/nginx start
-gunicorn -k uvicorn.workers.UvicornWorker -c /etc/gunicorn/gunicorn.conf.py ext.app:app
+gunicorn -k uvicorn.workers.UvicornWorker -c /etc/gunicorn/gunicorn.conf.py ext.app:app --log-level=$(echo ${LOG_LEVEL:-INFO} | tr '[:upper:]' '[:lower:]') --access-logfile - --access-logformat ""
--- sub/start.sh
+++ sub/start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+set -e
 /etc/init.d/nginx start
-gunicorn -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -c /etc/gunicorn/gunicorn.conf.py sub.app:app
+gunicorn -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -c /etc/gunicorn/gunicorn.conf.py sub.app:app --log-level=$(echo ${LOG_LEVEL:-INFO} | tr '[:upper:]' '[:lower:]') --access-logfile - --access-logformat ""
