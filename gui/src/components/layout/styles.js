9450224f7defb0fa5a22a59dfaf12f57ba28f38f
Nick Kunz <nick.kunz@me.com>
2024-03-11 01:04:20 -0700

    Add main layout styles for pages

--- gui/src/components/layout/styles.js
+++ gui/src/components/layout/styles.js
@@ -1,14 +1,34 @@
 import styled from "styled-components";
 import { v } from "../../styles/variables";
 
+export const TextContainer = styled.div`
+  width: 50%;
+`;
+
+export const HorizontalList = styled.ul`
+  display: flex;
+  justify-content: flex-start;
+  list-style-type: none;
+  padding: 0;
+
+  & > li:not(:last-child) {
+    margin-right: 32px;
+  }
+`;
+
+export const StyledLink = styled.a`
+  color: #DAA520; /* Dark yellow gold color */
+  text-decoration: none;
+`;
+
 export const SLayout = styled.div`
     display: flex;
 `;
 
 export const SMain = styled.main`
-    padding: calc(${v.smSpacing} * 2);
+    padding: calc(${v.smSpacing} * 3.2);
 
     h1 {
-        font-size: 14px;
+        font-size: 26px;
     }
 `;
